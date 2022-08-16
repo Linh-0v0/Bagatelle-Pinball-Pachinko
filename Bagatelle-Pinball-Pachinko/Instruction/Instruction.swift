a4bdb0f7441d36b7708c2cd4b6e49ff135dcfba4
Vu Bui Khanh Linh <73817943+Linh-0v0@users.noreply.github.com>
2022-08-16 13:41:48 +0700

    Change "Instruction"  rule

--- Bagatelle-Pinball-Pachinko/Instruction/Instruction.swift
+++ Bagatelle-Pinball-Pachinko/Instruction/Instruction.swift
@@ -10,7 +10,6 @@ import SpriteKit
 class Instruction: SKScene {
     
     var title, detail: SKLabelNode!
-    var title2, appName, course, yearPubl, location: SKLabelNode!
     
     override func didMove(to view: SKView) {
         //        let whiteBg = SKSpriteNode(imageNamed: "whiteBg")
@@ -32,43 +31,13 @@ class Instruction: SKScene {
         
         instructSection(titleStr: "How to Play",
                         titlePosition: CGPoint(x: 70, y: 1200),
-                        detailStr: "Each Ball costs 25 Coins. Get more money by Scoring MORE than the Coin you currently have. And see how much Coins you can Get or simply Game Over :)",
-                        detailPosition: CGPoint(x: 100, y: 940))
+                        detailStr: "- You initially have 125 coins in your pocket.\n- Each Ball costs 25 Coins.\n- Get more money by Scoring MORE than the Coin you currently have.\n- You're Game Over when you're out of Coin.\n- And WIN the game by reaching 1000 points. \n- Leaderboard ranks players by the least number of balls used to win the game.",
+                        detailPosition: CGPoint(x: 90, y: 600))
         
-        title2 = SKLabelNode(fontNamed: "Chalkduster")
-        title2.text = "Application Information"
-        title2.fontSize = 45
-        title2.horizontalAlignmentMode = .left
-        title2.position = CGPoint(x: 70, y: appDetailY)
-        addChild(title2)
-        
-        appName = SKLabelNode(fontNamed: "Chalkduster")
-        appName.text = "App Name: Pinball Pachinko"
-        appName.fontSize = appDetailSize
-        appName.horizontalAlignmentMode = .left
-        appName.position = CGPoint(x: 100, y: appDetailY - 80)
-        addChild(appName)
-        
-        course = SKLabelNode(fontNamed: "Chalkduster")
-        course.text = "Course: COSC2659"
-        course.fontSize = appDetailSize
-        course.horizontalAlignmentMode = .left
-        course.position = CGPoint(x: 100, y: appDetailY - 80*2)
-        addChild(course)
-        
-        yearPubl = SKLabelNode(fontNamed: "Chalkduster")
-        yearPubl.text = "Year Published: 2022"
-        yearPubl.fontSize = appDetailSize
-        yearPubl.horizontalAlignmentMode = .left
-        yearPubl.position = CGPoint(x: 100, y: appDetailY - 80*3)
-        addChild(yearPubl)
-        
-        location = SKLabelNode(fontNamed: "Chalkduster")
-        location.text = "Location: Saigon South Campus"
-        location.fontSize = appDetailSize
-        location.horizontalAlignmentMode = .left
-        location.position = CGPoint(x: 100, y: appDetailY - 80*4)
-        addChild(location)
+        instructSection(titleStr: "Application Information",
+                        titlePosition: CGPoint(x: 70, y: 460),
+                        detailStr: "- App Name: Pinball Pachinko \n- Course: COSC2659 \n- Year Published: 2022 \n- Location: Saigon South Campus",
+                        detailPosition: CGPoint(x: 100, y: 200))
     }
     
     func instructSection(titleStr: String, titlePosition: CGPoint, detailStr: String, detailPosition: CGPoint) {
@@ -81,9 +50,9 @@ class Instruction: SKScene {
         
         detail = SKLabelNode(fontNamed: "Chalkduster")
         detail.text = detailStr
-        detail.fontSize = 32
+        detail.fontSize = 35
         detail.preferredMaxLayoutWidth = self.size.width - 130
-        detail.numberOfLines = 7
+        detail.numberOfLines = 15
         detail.horizontalAlignmentMode = .left
         detail.position = detailPosition
         addChild(detail)
